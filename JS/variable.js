79d57b5feb2b1b79f0636c195ad6872d18457963
htheresatran <63614161+htheresatran@users.noreply.github.com>
2020-05-05 13:53:39 -0700

    May 5th 2020

---
+++ JS/variable.js
@@ -0,0 +1,19 @@
+// console.log("Hello World");
+
+/*
+trwsfjidoijef th
+*/
+
+//global
+var a=5;
+
+
+
+console.log(a);
+
+function apple() {
+    //local
+    var a=50;
+    return a;
+}
+console.log(apple())
