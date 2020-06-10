2b16de07f009d184e528f7241c2e5c4e4da83d41
htheresatran <63614161+htheresatran@users.noreply.github.com>
2020-06-09 23:07:19 -0700

    itcc

--- classEx/week 08/calculator/index.js
+++ classEx/week 08/calculator/index.js
@@ -4,13 +4,54 @@ function loadDisplay(num) {
 }
 function delFromRight() {
     document.getElementById('display2').value = document.getElementById('display2').value.slice(0,-1);
-}
-function clearDisplay() {
-    document.getElementById('display2').value = "";
 
 }
-function setOperator(trg) {
-    val1 = document.getElementById('display2').value + "" + trg.value;
+function clearDisplay(trg){
+    document.getElementById('display2').value + " " + trg.value;
     document.getElementById('display1').innerHTML = val1;
     document.getElementById('display2').value = "";
 }
+function setOperator(trg) {
+    val1 = document.getElementById('display2').value + " " + trg.value;
+    document.getElementById('display1').innerHTML = val1;
+    document.getElementById('display2').value = "";
+}
+
+
+
+
+function doMath(){
+    val2 = document.getElementById('display2').value;
+    var result;
+    switch(val1.slice(-1)){
+        case '+':
+            result = parseInt(val1) + parseInt(val2);
+            break;
+        case '-':
+            result = parseInt(val1) - parseInt(val2);
+            break;
+        case '*':
+            result = parseInt(val1) * parseInt(val2);
+            break;
+        case '/':
+            result = parseInt(val1) / parseInt(val2);
+            break;
+        case '√':
+            result = Math.sqrt(parseInt(val1));
+            break;
+        case 'pow':
+            result = Math.pow(parseInt(val1), parseInt(val2));
+            break;
+
+        }
+
+        document.getElementById('display1').innerHTML = "";
+        document.getElementById('display2').value = result;
+    }
+//function specialCase(){
+   // if(sqrt){
+        //do square root math
+    //}else{
+        //do exponent math
+//}
+//}
